0a110d69323a075fa09315301c0b89ce352214ce
Ziqing Wang <redtaq@hotmail.com>
2018-04-11 02:10:25 +0000

    tornado-blog

--- controller.py
+++ controller.py
@@ -3,6 +3,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.log
 import queries
+import time
 
 from jinja2 import \
   Environment, PackageLoader, select_autoescape
@@ -28,20 +29,63 @@ class TemplateHandler(tornado.web.RequestHandler):
 class MainHandler(TemplateHandler):
     def get(self):
         super().get()
-        posts = self.session.query('SELECT blog.title AS blog_title, blog.slug AS blog_slug, blog.publish_date, authors.name AS author_name FROM blog INNER JOIN authors ON blog.author_id=authors.id ORDER BY blog.publish_date')
+        posts = self.session.query('SELECT blog.title AS blog_title, blog.slug AS blog_slug, blog.publish_date, authors.name AS author_name FROM blog INNER JOIN authors ON blog.author_id=authors.id ORDER BY blog.publish_date DESC')
         self.render_template('main.html', {'posts': posts})
         
 class PageHandler(TemplateHandler):
     def get(self, slug):
         super().get()
-        post = self.session.query('SELECT blog.title AS blog_title, blog.slug AS blog_slug, blog.contents, blog.publish_date, authors.name AS author_name FROM blog INNER JOIN authors ON blog.author_id=authors.id WHERE blog.slug = %(slug)s', {'slug': slug})
-        self.render_template('post_page.html', {'post': post[0]})
+        comments = self.session.query('''
+                SELECT comment_date, blog.title AS blog_title, authors.name AS blog_author_name, blog.publish_date AS blog_publish_date, blog.contents AS blog_contents, comments.comment_author AS comment_name, comments.contents AS comment_contents FROM blog INNER JOIN authors ON blog.author_id=authors.id JOIN comments ON comments.blog_id=blog.id WHERE blog.slug = %(slug)s ORDER BY comment_date DESC;
+            ''', {'slug': slug})
+        if len(comments) > 0:
+            self.render_template('post_page.html', {'comments': comments})    
+        else:
+            post = self.session.query('''
+            SELECT blog.title AS blog_title, authors.name AS blog_author_name, blog.publish_date AS blog_publish_date, blog.contents AS blog_contents FROM blog 
+            INNER JOIN authors ON blog.author_id=authors.id WHERE blog.slug=%(slug)s
+            ''', {'slug': slug})
+            self.render_template('single_page.html', {'post': post})
 
-
+        
+    def post(self, slug):
+        new_comment = self.get_body_argument('new_comment', None)
+        name = self.get_body_argument('name', None)
+        ts = time.gmtime()
+        fmt = '%Y-%m-%d %H:%M:%S'
+        comment_time = time.strftime(fmt, ts)
+        post_id = int(slug[3:])
+        addComment = self.session.query('INSERT INTO comments VALUES (DEFAULT, %(comment_time)s, %(new_comment)s, %(post_id)s, %(name)s)', {'comment_time': comment_time, 'new_comment': new_comment, 'post_id': post_id, 'name': name})
+        self.get(slug)
+        
+class ArchiveHandler(TemplateHandler):
+    def get(self, category):
+        super().get()
+        posts = self.session.query('SELECT blog.title AS blog_title, blog.slug AS blog_slug, blog.category AS blog_category, blog.publish_date, authors.name AS author_name FROM blog INNER JOIN authors ON blog.author_id=authors.id WHERE blog.category=%(category)s ORDER BY blog.publish_date DESC', {'category': category})
+        self.render_template('main.html', {'posts': posts})
+        
+class NewpostHandler(TemplateHandler):
+    def get(self):
+        super().get()
+        self.render_template('newpost.html', {})
+        
+    def post(self):
+        new_post = self.get_body_argument('new_content', None)
+        title = self.get_body_argument('title', None)
+        category = self.get_body_argument('category', None)
+        ts = time.gmtime()
+        fmt = '%Y-%m-%d %H:%M:%S'
+        slug = self.get_body_argument('slug', None)
+        comment_time = time.strftime(fmt, ts)
+        addPost = self.session.query('INSERT INTO blog VALUES (DEFAULT, %(comment_time)s, %(title)s, %(slug)s, %(contents)s, 1, %(category)s)', {'comment_time': comment_time, 'title': title, 'slug': slug, 'contents': new_post, 'category': category})
+        
+    
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/post/(.*)", PageHandler),
+        (r"/list/(.*)", ArchiveHandler),
+        (r"/create_post", NewpostHandler),
         (r"/static/(.*)", tornado.web.StaticFileHandler,
         {'path': 'blog/static'})
         ],
